5cde7ea9f3a3472b43e53364af3dc997a5ca0485
Ian Salmons <ian.salmons+github@gmail.com>
2014-05-17 20:23:24 +0100

    Part 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
+	public float speed;
 
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horinzontal");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rigidbody.AddForce (movement);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
 	}
 
 }
