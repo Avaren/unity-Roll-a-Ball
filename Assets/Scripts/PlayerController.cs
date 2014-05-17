d0c201bc9c3d4bb61f4033284942006ee69ed920
Ian Salmons <ian.salmons+github@gmail.com>
2014-05-17 20:14:07 +0100

    Part 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horinzontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement);
+	}
+
+}
